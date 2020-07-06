6b33158b26fb19b0cf9d89a373d1c122a0f421a6
sedrik1 <32675171+sedrik1@users.noreply.github.com>
2020-07-07 00:19:56 +0300

    Parantelu #3

--- js.js
+++ js.js
@@ -14,18 +14,15 @@ function changeLang(language) {
     const h2FI = ['Yhden sivun kauppasovellus (SPA)', 'Tietotekniikkasanakartuttaja', 'c'];
     const h2E = ['Single-page application shop', 'IT word accumulator', 'c'];
     const finD = [
-        `SPA-periaattella toimiva kauppa. Erittäin opettavainen projekti JS-kehitysraamien mahdollisuuksista ja niissä käytetyistä kehitysperiaatteista, kuten moduuleista holistiselta kannalta sekä jaettuna komponentteihin, palveluihin, reittivahteihin (route guard) jne., RxJS-kirjaston erinomaisuudesta ja JSON Web Tokeneista.`,
-
         `Kokonaan itse tehty palvelu PHP:llä, JavaScriptillä ja SQL:llä. Ohjelmistoratkaisupinona (solution stack) käytin XAMPP:ia, joka kätevästi tarjoaa paikallisen Apache-palvelimen ja MySQL-relaatiotietokannan. Projektin aihe oli luoda palvelu suomenkielisiä tietotekniikkasanoja varten, koska oman kokemukseni mukaan kotimainen termistö on käytännössä kokonaan sivuutettu anglismien hyödyntämisen puolesta. 
         
         Projekti antoi hyvän läpileikkauksen selain- ja palvelinpuolen toiminnasta, sillä minun oli luotava kaikki toiminnallisuus tietokannasta datansiirtoon PHP:llä ja siitä AJAX:in avulla selainpuolelle ja takaisin tietokantaan. Lisäksi rekisteröityminen ja siten tilien henkilökohtaisten sanasyötteiden ongelmien ratkominen oli mukavaa aivojumppaa. 
         
         Muita ominaisuuksia ovat sähköpostiosoitteen vaihtaminen, tilin poistaminen, lempisanojen tallentaminen profiilisivulle ja sieltä poistaminen, lempisanojen lataaminen Excel-tiedostona, sanojen etsiminen tietokannasta ja sanavisa. Tiedonkeruun suoritin PHP:llä web-scrapingiä (internetsivujen siivilöintiä) käyttäen.`,
-        ``
+        `SPA-periaattella toimiva kauppa. Erittäin opettavainen projekti JS-kehitysraamien mahdollisuuksista ja niissä käytetyistä kehitysperiaatteista, kuten moduuleista holistiselta kannalta sekä jaettuna komponentteihin, palveluihin, reittivahteihin (route guard) jne., RxJS-kirjaston erinomaisuudesta ja JSON Web Tokeneista.`,
+        `c`
     ];
     const engD = [
-        `A store page made following SPA principles. A great project about learning and understanding modern JS frameworks' potentials and their development tenets, like modules in a holistic and individual way, like components, services, auth guards, the RxJS library and JSON Web Tokens`,
-
         `A service I made completely by myself from the ground-up. I utilised PHP, JavaScript and SQL with XAMPP as the web server solution stack. XAMPP niftily comes pre-packaged with
         a local Apache server as well as a MySQL relational database. The subject of the project was an information technology term service in Finnish. 
 
@@ -33,6 +30,7 @@ function changeLang(language) {
         All communication from the database via PHP passed on by AJAX to the client side and then back to MySQL really gave a clear view of the front and back end operations and how they interact with each other.
         
         Functions of the the website include: changing the email, deleting an account, saving favourite words to the profile page and deleting them from there, downloading favourite words as an Excel file, searching for words from the database and word quiz. To gather information I used PHP to web-scrape pages for words and definitions.`,
+        `A store page made following SPA principles. A great project about learning and understanding modern JS frameworks' potentials and their development tenets, like modules in a holistic and individual way, like components, services, auth guards, the RxJS library and JSON Web Tokens.`,
         `c`
     ];
     let links = getLinks();
